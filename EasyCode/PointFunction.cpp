b8c613781732c52cc3e23204c6661ddac6b2af9e
Youmo <32959859+Youmo742@users.noreply.github.com>
2017-11-11 18:05:08 +0800

    Create PointFunction.cpp

---
+++ EasyCode/PointFunction.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int arr(const int *arr, int n)
+{
+	int sum = 0;
+	for (int i = 0;i < n;i++)
+	{
+		cout << " " << *arr;
+		sum += *(arr++);
+		
+	}
+	return sum;
+}
+int main1()
+{
+	const int n = 5;
+	int a[n] = { 1,2,3,4,5 };
+	cout << ' '<<(arr(a, n));
+	cin.get();
+	return 0;
+}
