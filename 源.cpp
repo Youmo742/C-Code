9bac4fff2b7a60716705374690d258fb11541e3a
Youmo <32959859+Youmo742@users.noreply.github.com>
2017-11-11 17:56:07 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,178 +1,36 @@
-#include <windows.h> 
-#include <iostream>
-#include <string>
-#include <ctype.h>
-using namespace std;
-
-typedef int dataType;
-
-typedef struct siglist
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define time 5.9
+void total()
 {
-	dataType data;
-	struct siglist *next;
-}sList;
-
-void add_data_to_list_tail(sList **head, dataType *data)
-{
-	sList *p = *head;
-	sList *newList = NULL;
-
-	newList = (sList *)malloc(sizeof(sList));
-	newList->data = *data;
-	newList->next = NULL;
-
-	if ((head == NULL)
-		|| (*head == NULL))
+	int n = 0;
+	int i = 0,t=0;
+	double totime[33] = { 0 };
+	int index1 = 0,index2=0;
+	int a[33];
+	while(scanf("%d",&n))
 	{
-		*head = newList;
-	}
-	else
-	{
-		while (p->next != NULL)
-		{
-			p = p->next;
-		}
-		p->next = newList;
-	}
-}
-
-void delete_list_data(sList **head, dataType *data)
-{
-	sList *p = *head;
-	sList *prevNode = NULL;
-	if ((head != NULL)
-		&& (*head != NULL))
-	{
-		if (p->data == *data)
-		{
-			*head = p->next;
-			free(p);
-		}
-		else
-		{
-			prevNode = p;
-			p = p->next;
-			while ((p != NULL)
-				&& (p->data != *data))
+		if (n == 0)
+			break;
+		a[0] = 0;
+			for (index1 = 1;index1 <= n;index1++)
 			{
-				prevNode = p;
-				p = p->next;
+				scanf("%d", &a[index1]);
 			}
-			if (p != NULL)
+			for (index2 = 1;index2 <= n;index2++)
 			{
-				prevNode->next = p->next;
-				free(p);
+				totime[i] += ((abs((a[index2] - a[index2 - 1])))*time);
 			}
-		}
+			i++;
 	}
+	while(t<i)
+	printf("%.2f\n", totime[t++]);
 }
-
-void add_list_to_tail(sList **head, sList *list)
+int main()
 {
-	sList *p = *head;
-	if (p == NULL)
-	{
-		*head = list;
-	}
-	else
-	{
-		while (p->next != NULL)
-		{
-			p = p->next;
-		}
-		p->next = list;
-	}
-}
-
-void sort_list(sList **head)
-{
-	if ((head == NULL)
-		|| (*head == NULL)
-		|| ((*head)->next == NULL))
-	{
-		return;
-	}
-
-	sList *tmpHead = NULL;
-	sList *tmp = NULL;
-	sList *smallNode = NULL;
-	sList *prevNode = NULL;
-	sList *nextNode = NULL;
-
-	prevNode = *head;
-	smallNode = *head;
-	nextNode = *head;
-	while (prevNode->next != NULL)
-	{
-		while (nextNode->next != NULL)
-		{
-			if (nextNode->next->data < smallNode->next->data)
-			{
-				smallNode = nextNode;
-			}
-			nextNode = nextNode->next;
-		}
-
-		if (smallNode != prevNode)
-		{
-			add_list_to_tail(&tmpHead, smallNode->next);
-			/*delete the node from the list*/
-			tmp = smallNode->next;
-			smallNode->next = smallNode->next->next;
-			tmp->next = NULL;
-		}
-		else if (prevNode->next->data < smallNode->data)
-		{
-			add_list_to_tail(&tmpHead, smallNode->next);
-			/*delete the node from the list*/
-			tmp = smallNode->next;
-			smallNode->next = smallNode->next->next;
-			tmp->next = NULL;
-		}
-		else
-		{
-			smallNode = *head;
-			*head = (*head)->next;
-			add_list_to_tail(&tmpHead, smallNode);
-			/*delete the node from the list*/
-			tmp = smallNode->next;
-			smallNode->next = smallNode->next->next;
-			tmp->next = NULL;
-		}
-		prevNode = *head;
-		smallNode = *head;
-		nextNode = *head;
-	}
-	add_list_to_tail(&tmpHead, prevNode);
-	*head = tmpHead;
-}
-
-void main(void)
-{
-	sList *list = NULL;
-	dataType data;
-	//dataType data[] = { 56,35,88,44,34,65,1,8 };
-
-	int index = 0;
-	/*for (index = 0;index < sizeof(data) / sizeof(dataType);index++)
-	{
-		add_data_to_list_tail(&list, &(data[index]));
-	}*/
-	cin >> data;
-	while ( data)
-	{
-		cin >> data;
-		add_data_to_list_tail(&list, &data);
-		
-	}
-
-	sort_list(&list);
-
-	sList *p = list;
-	while (p)
-	{
-		cout << p->data << " ";
-		p = p->next;
-	}
-	system("PAUSE");
+	total();
+	system("pause");
+	return 0;
 }
